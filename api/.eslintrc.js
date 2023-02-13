93375c07519ba1e777d76bb4e87c6a8ae0458b88
Br0k3N-ET3rNaL <tyliewilie99@gmail.com>
2023-02-13 17:40:52 -0600

    Update .eslintrc.js

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,21 +1,27 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-  },
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+    ],
+    overrides: [
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: [
+        'react',
+    ],
+    rules: {
+        'no-console': 0,
+        'no-unused-vars': 1,
+        'no-empty-function': 1,
+        'class-methods-use-this': 1,
+        'linebreak-style': 0,
+        indent: ['error', 4],
+    },
 };
