5593570606d01e69ca518d0fb4b0c88e3f1f9e13
Br0k3N-ET3rNaL <tyliewilie99@gmail.com>
2023-02-15 14:29:48 -0600

    Add Database Config for Sequelize
    Closes #23

---
+++ api/config/db.config.js
@@ -0,0 +1,33 @@
+const { Sequelize, Model, DataTypes } = require('sequelize');
+
+const connect = () => {
+    const hostName = 'localhost';
+    const userName = 'postgres';
+    const password = 'password';
+    const database = 'watchlist';
+    const dialect = 'postgres';
+
+    const sequelize = new Sequelize(database, userName, password, {
+        host: hostName,
+        dialect,
+        operatorsAliases: false,
+        pool: {
+            max: 10,
+            min: 0,
+            acquire: 20000,
+            idle: 5000,
+        },
+    });
+
+    const db = {};
+    db.Sequelize = Sequelize;
+    db.sequelize = sequelize;
+    // eslint-disable-next-line global-require
+    db.user = require('../model/user.model')(sequelize, DataTypes, Model);
+
+    return db;
+};
+
+module.exports = {
+    connect,
+};
