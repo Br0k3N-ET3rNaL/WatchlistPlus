2a866c71009348beba5021e288686f1a1c76ce27
Br0k3N-ET3rNaL <tyliewilie99@gmail.com>
2023-02-15 14:24:48 -0600

    Update .eslintrc.js

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
         'no-console': 0,
         'no-unused-vars': 1,
         'no-empty-function': 1,
-        'class-methods-use-this': 1,
+        'class-methods-use-this': 0,
         'linebreak-style': 0,
         indent: ['error', 4],
     },
