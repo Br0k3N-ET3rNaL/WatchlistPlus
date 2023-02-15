b39c1d492c5f18098e73f1b2cfd49b6f6b87cbec
Br0k3N-ET3rNaL <tyliewilie99@gmail.com>
2023-02-15 14:48:29 -0600

    Update Topbar
    Topbar can now be used to switch between views

--- ui/src/_codux/boards/top-bar/top-bar.board.tsx
+++ ui/src/_codux/boards/top-bar/top-bar.board.tsx
@@ -1,9 +1,9 @@
 import { createBoard } from '@wixc3/react-board';
-import { TopBar } from '../../../components/top-bar/top-bar';
+import TopBar from '../../../components/top-bar/top-bar';
 
 export default createBoard({
     name: 'TopBar',
-    Board: () => <TopBar />,
+    Board: () => <TopBar home={true}/>,
     environmentProps: {
         canvasWidth: 1064,
         canvasHeight: 723,
--- ui/src/components/top-bar/top-bar.tsx
+++ ui/src/components/top-bar/top-bar.tsx
@@ -1,29 +1,59 @@
 import styles from './top-bar.module.scss';
 import classNames from 'classnames';
+import React from 'react';
 
-export interface TopBarProps {
+type TopBarProps = {
     className?: string;
     children?: React.ReactNode;
+    signup?: () => void;
+    login?: () => void;
+    back?: (loggedIn: boolean) => void;
+    home: boolean;
+    loggedIn?: boolean;
 }
 
 /**
  * This component was generated using Codux's built-in Default new component template.
  * For details on how to create custom new component templates, see https://help.codux.com/kb/en/article/configuration-for-top-bars-and-templates
  */
-export const TopBar = ({ className, children = 'TopBar' }: TopBarProps) => {
-    return (
-        <div className={classNames(styles.root, className)}>
-            <div className={classNames('topbarLeft', styles.topbarLeft)}>
+class TopBar extends React.Component<TopBarProps> {
+    render() {
+        return (
+            <div className={classNames(styles.root, this.props.className)}>
+                <div className={classNames('topbarLeft', styles.topbarLeft)}>
+                    <span className={classNames('logo', styles.logo)}></span>
+                </div>
                 <span className={classNames('logo', styles.logo)}></span>
-            </div>
-            <span className={classNames('logo', styles.logo)}></span>
-            <div className={classNames('topbarCenter', styles.topbarCenter)}></div>
-            <div className={classNames('topbarRight', styles.topbarRight)}>
-                <div className={'topbarLinks'}>
-                    <span className={classNames('topbarLink', styles.topbarLink)}>Sign Up</span>
-                    <span className={classNames('topbarLink', styles.topbarLink)}>Log in</span>
+                <div className={classNames('topbarCenter', styles.topbarCenter)}></div>
+                <div className={classNames('topbarRight', styles.topbarRight)}>
+                    {(this.props.home && !this.props.loggedIn) && <div className={'topbarLinks'}>
+                        <span
+                            className={classNames('topbarLink', styles.topbarLink)}
+                            onClick={this.props.signup}
+                        >
+                            Sign Up
+                        </span>
+                        <span
+                            className={classNames('topbarLink', styles.topbarLink)}
+                            onClick={this.props.login}
+                        >
+                            Log In
+                        </span>
+                    </div>}
+                    {(!this.props.home && !this.props.loggedIn) && <span
+                        className={classNames('topbarLink', styles.topbarLink)}
+                        onClick={() => {
+                            if (this.props.back !== undefined) {
+                                this.props.back(false)
+                            }
+                        }}
+                    >
+                        Go Back
+                    </span>}
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 };
+
+export default TopBar;
