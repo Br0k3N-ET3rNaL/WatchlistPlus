4ba07ada53366a57011146187d716f42fcd6a806
Br0k3N-ET3rNaL <tyliewilie99@gmail.com>
2023-02-15 14:36:01 -0600

    Add Logger Class

---
+++ api/logger/api.logger.js
@@ -0,0 +1,25 @@
+const winston = require('winston');
+
+const consoleTransport = new winston.transports.Console();
+const myWinstonOptions = {
+    transports: [consoleTransport],
+};
+// eslint-disable-next-line new-cap
+const logger = new winston.createLogger(myWinstonOptions);
+
+class APILogger {
+    info(message) {
+        logger.info(message);
+    }
+
+    // eslint-disable-next-line no-dupe-class-members
+    info(message, data) {
+        logger.info(`${message}   ${undefined !== data ? JSON.stringify(data) : ''}`);
+    }
+
+    error(message) {
+        logger.error(message);
+    }
+}
+
+module.exports = new APILogger();
