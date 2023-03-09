06a5d8bd60080148b62726d57e8949a287f04185
Br0k3N-ET3rNaL <tyliewilie99@gmail.com>
2023-03-08 20:02:37 -0600

    Update Title Repository Test
    Changed how the database is being mocked, again

--- api/test/title.repository.test.js
+++ api/test/title.repository.test.js
@@ -1,33 +1,27 @@
 const titleRepository = require('../repository/title.repository');
 const logger = require('../logger/api.logger');
-const { connect } = require('../config/db.config');
 
-// eslint-disable-next-line no-unused-vars
-// const db = jest.mock('../config/db.config', () => ({
-//     connect: jest.fn(),
-// }));
+jest.mock('../config/db.config', () => ({
+    connect: () => {
+        const db = {};
+        db.title = {};
+        db.title.rawAttributes = {};
+        db.title.rawAttributes.title = true;
+        db.title.rawAttributes.notAColumn = false;
+        db.sequelize = { query: jest.fn() };
+
+        return db;
+    },
+}));
 
 logger.error = jest.fn();
 
-beforeEach(() => {
-    jest.resetModules();
-});
-
-// test('moduleName 1', () => {
-//     jest.doMock('../config/db.config', () => jest.fn());
-//     // eslint-disable-next-line global-require
-//     const moduleName = require('../config/db.config');
-//     expect(moduleName()).toEqual(1);
-// });
-
 test('valid sortColumn', async () => {
-    jest.doMock('../config/db.config', () => jest.fn());
     await titleRepository.getPageOfTitles(50, 1, 'title', '');
     expect(logger.error.mock.calls).toHaveLength(0);
 });
 
 test('invalid sortColumn', async () => {
-    jest.doMock('../config/db.config', () => jest.fn());
     await titleRepository.getPageOfTitles(50, 1, 'notAColumn', '');
     expect(logger.error.mock.calls).toHaveLength(1);
     expect(logger.error.mock.calls[0][0]).toBe('Error::notAColumn does not exists in titles');
