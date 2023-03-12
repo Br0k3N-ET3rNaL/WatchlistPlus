ac70bfb767aaf8d434a2dd13f2470f81db592d1c
Br0k3N-ET3rNaL <tyliewilie99@gmail.com>
2023-03-12 15:41:01 -0500

    Create Watchlist List Element Component
    Closes #32

---
+++ ui/src/components/watchlist-list-element/watchlist-list-element.tsx
@@ -0,0 +1,32 @@
+import styles from './watchlist-list-element.module.scss';
+import classNames from 'classnames';
+import React from 'react';
+
+type WatchlistListElementProps = {
+    className?: string;
+    children?: React.ReactNode;
+    key: number;
+    watched: {rating: number, status: string};
+    title: { title: string, type: string, description: string, releaseYear: number, ageGuidance: string, runtime: number, rating: number, genres: string[] };
+    displayTitle?: (title: { title: string, type: string, description: string, releaseYear: number, ageGuidance: string, runtime: number, rating: number, genres: string[] }) => void;
+}
+
+class WatchlistListElement extends React.Component<WatchlistListElementProps> {
+    render() {
+        return (
+            <div className={classNames(styles.root, this.props.className)}>{this.props.children}
+                <div />
+                <div className={styles.title} onClick={() => {
+                    if (this.props.displayTitle !== undefined)
+                        this.props.displayTitle(this.props.title)
+                }}> {this.props.title.title} </div>
+                <div> {this.props.watched.status} </div>
+                <div> {this.props.title.releaseYear} </div>
+                <div> {this.props.watched.rating} </div>
+            </div>
+        );
+    }
+
+};
+
+export default WatchlistListElement;
