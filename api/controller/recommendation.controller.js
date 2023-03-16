04da5b3c81ec597b3c6cd842d1b52662b8c2d023
Br0k3N-ET3rNaL <tyliewilie99@gmail.com>
2023-03-16 10:55:51 -0500

    Update recommendation.controller.js
    Update logging

--- api/controller/recommendation.controller.js
+++ api/controller/recommendation.controller.js
@@ -3,8 +3,11 @@ const logger = require('../logger/api.logger');
 
 class RecommendationController {
     async createRecommendation(recommendation) {
-        logger.info('RecommendationController: createRecommendation');
-        return recommendationService.createReview(recommendation);
+        logger.info(
+            'RecommendationController: createRecommendation',
+            `${recommendation.title1Id} ${recommendation.title2Id} ${recommendation.userId}`,
+        );
+        return recommendationService.createRecommendation(recommendation);
     }
 
     async getPageOfRecommendations(titleId, pageLength, pageNum) {
