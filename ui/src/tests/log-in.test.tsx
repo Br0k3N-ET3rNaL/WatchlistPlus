859c02004008c75a59a8dcd624191eb3aeb4a768
Br0k3N-ET3rNaL <tyliewilie99@gmail.com>
2023-03-17 13:48:10 -0500

    Fix Unit Tests
    Fix some assertions not getting called in unit tests.
    Fixes #66

--- ui/src/tests/log-in.test.tsx
+++ ui/src/tests/log-in.test.tsx
@@ -1,57 +1,55 @@
 import LogIn from '../components/log-in/log-in';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-test('no user with email', async () => {
-    render(<LogIn />);
+describe('log-in', () => {
+    test('no user with email', async () => {
+        render(<LogIn />);
 
-    await userEvent.type(screen.getByRole('textbox', { name: /email/i }), 'email@gmail.com');
-    await userEvent.type(screen.getByLabelText(/password/i), 'password');
+        await userEvent.type(screen.getByRole('textbox', { name: /email/i }), 'email@gmail.com');
+        await userEvent.type(screen.getByLabelText(/password/i), 'password');
 
-    global.fetch = jest.fn(async () =>
-        Promise.resolve({ json: async () => Promise.resolve({ userExists: false, passwordsMatch: true }) })
-    ) as jest.Mock;
+        global.fetch = jest.fn(async () =>
+            Promise.resolve({ json: async () => Promise.resolve({ userExists: false, passwordsMatch: true }) })
+        ) as jest.Mock;
 
-    await userEvent.click(screen.getByRole('button', { name: /log in/i }));
+        await userEvent.click(screen.getByRole('button', { name: /log in/i }));
 
-    setTimeout(() => {
-        expect(screen.queryByText('* No user with given email')!.hidden).toBeFalsy();
+        await waitFor(() => expect(screen.queryByText('* No user with given email')!.hidden).toBeFalsy());
         expect(screen.getByText('* Incorrect Password').hidden).toBeTruthy();
-    }, 1000);
-});
 
-test('valid user wrong password', async () => {
-    render(<LogIn />);
 
-    await userEvent.type(screen.getByRole('textbox', { name: /email/i }), 'email@gmail.com');
-    await userEvent.type(screen.getByLabelText(/password/i), 'password');
+    });
 
-    global.fetch = jest.fn(async () =>
-        Promise.resolve({ json: async () => Promise.resolve({ userExists: true, passwordsMatch: false }) })
-    ) as jest.Mock;
+    test('valid user wrong password', async () => {
+        render(<LogIn />);
 
-    await userEvent.click(screen.getByRole('button', { name: /log in/i }));
+        await userEvent.type(screen.getByRole('textbox', { name: /email/i }), 'email@gmail.com');
+        await userEvent.type(screen.getByLabelText(/password/i), 'password');
 
-    setTimeout(() => {
-        expect(screen.getByText('* No user with given email').hidden).toBeTruthy();
-        expect(screen.queryByText('* Incorrect Password')!.hidden).toBeFalsy();
-    }, 1000);
-});
+        global.fetch = jest.fn(async () =>
+            Promise.resolve({ json: async () => Promise.resolve({ userExists: true, passwordsMatch: false }) })
+        ) as jest.Mock;
 
-test('valid user correct password', async () => {
-    render(<LogIn />);
+        await userEvent.click(screen.getByRole('button', { name: /log in/i }));
 
-    await userEvent.type(screen.getByRole('textbox', { name: /email/i }), 'email@gmail.com');
-    await userEvent.type(screen.getByLabelText(/password/i), 'password');
+        await waitFor(() => expect(screen.getByText('* No user with given email').hidden).toBeTruthy());
+        await waitFor(() => expect(screen.queryByText('* Incorrect Password')!.hidden).toBeFalsy());
+    });
 
-    global.fetch = jest.fn(async () =>
-        Promise.resolve({ json: async () => Promise.resolve({ userExists: true, passwordsMatch: true }) })
-    ) as jest.Mock;
+    test('valid user correct password', async () => {
+        render(<LogIn />);
 
-    await userEvent.click(screen.getByRole('button', { name: /log in/i }));
+        await userEvent.type(screen.getByRole('textbox', { name: /email/i }), 'email@gmail.com');
+        await userEvent.type(screen.getByLabelText(/password/i), 'password');
 
-    setTimeout(() => {
-        expect(screen.getByText('* No user with given email').hidden).toBeTruthy();
-        expect(screen.getByText('* Incorrect Password').hidden).toBeTruthy();
-    }, 1000);
+        global.fetch = jest.fn(async () =>
+            Promise.resolve({ json: async () => Promise.resolve({ userExists: true, passwordsMatch: true }) })
+        ) as jest.Mock;
+
+        await userEvent.click(screen.getByRole('button', { name: /log in/i }));
+
+        await waitFor(() => expect(screen.getByText('* No user with given email').hidden).toBeTruthy());
+        await waitFor(() => expect(screen.getByText('* Incorrect Password').hidden).toBeTruthy());
+    });
 });
--- ui/src/tests/sign-up.test.tsx
+++ ui/src/tests/sign-up.test.tsx
@@ -1,96 +1,100 @@
 import SignUp from "../components/sign-up/sign-up";
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-test('invalid username', async () => {
-    render(<SignUp />);
+describe('sign-up', () => {
+    test('invalid username', async () => {
+        render(<SignUp />);
 
-    global.fetch = jest.fn(async () =>
-        Promise.resolve({ json: async () => Promise.resolve({ usernameExists: true }) })
-    ) as jest.Mock;
+        const fetchMock = global.fetch = jest.fn(async () =>
+            Promise.resolve({ json: async () => Promise.resolve({ usernameExists: true }) })
+        ) as jest.Mock;
 
-    await userEvent.type(screen.getByRole('textbox', { name: /username/i }), 'username');
+        await userEvent.type(screen.getByRole('textbox', { name: /username/i }), 'username{enter}');
 
-    setTimeout(async () => {
-        expect(screen.queryByText('* Username already exists')!.hidden).toBeFalsy();
-    }, 2000);
+        await waitFor(() => expect(fetchMock).toHaveBeenCalled());
+        expect(fetchMock.mock.lastCall[0]).toBe('/api/users/exists/username/username');
+        expect(fetchMock.mock.lastCall[1].method).toBe('GET');
+
+        await waitFor(() => expect(screen.queryByText('* Username already exists')!.hidden).toBeFalsy());
+    });
+
+    test('invalid email', async () => {
+        render(<SignUp />);
+
+        const fetchMock = global.fetch = jest.fn(async () =>
+            Promise.resolve({ json: async () => Promise.resolve({ emailExists: true }) })
+        ) as jest.Mock;
+
+        await userEvent.type(screen.getByRole('textbox', { name: /email/i }), 'email@gmail.com{enter}');
+
+        await waitFor(() => expect(fetchMock).toHaveBeenCalled());
+        expect(fetchMock.mock.lastCall[0]).toBe('/api/users/exists/email/email@gmail.com');
+        expect(fetchMock.mock.lastCall[1].method).toBe('GET');
+
+        await waitFor(() => expect(screen.queryByText('* Email already exists')!.hidden).toBeFalsy());
+    });
+
+    test('passwords match', async () => {
+        render(<SignUp />);
+
+        await userEvent.type(screen.getByLabelText(/password/i), 'password{enter}');
+        await userEvent.type(screen.getByLabelText(/repeat/i), 'password{enter}');
+
+        await waitFor(() => expect(screen.getByText('* Passwords don\'t match').hidden).toBeTruthy());
+    });
+
+    test('passwords don\'t match', async () => {
+        render(<SignUp />);
+
+        await userEvent.type(screen.getByLabelText(/password/i), 'password{enter}');
+        await userEvent.type(screen.getByLabelText(/repeat/i), 'different{enter}');
+
+        await waitFor(() => expect(screen.queryByText('* Passwords don\'t match')!.hidden).toBeFalsy());
+    });
+
+    test('create user', async () => {
+        const testUser = { username: 'Test User', email: 'email@gmail.com', password: 'password' };
+
+        const login = jest.fn();
+
+        render(<SignUp login={login}/>);
+
+        let fetchMock = global.fetch = jest.fn(async () =>
+            Promise.resolve({ json: async () => Promise.resolve({ usernameExists: false }) })
+        ) as jest.Mock;
+
+        await userEvent.type(screen.getByRole('textbox', { name: /username/i }), testUser.username + '{enter}');
+
+        await waitFor(() => expect(fetchMock).toHaveBeenCalled());
+        await waitFor(() => expect(screen.getByText('* Username already exists').hidden).toBeTruthy());
+
+        fetchMock = global.fetch = jest.fn(async () =>
+            Promise.resolve({ json: async () => Promise.resolve({ emailExists: false }) })
+        ) as jest.Mock;
+
+        await userEvent.type(screen.getByRole('textbox', { name: /email/i }), testUser.email + '{enter}');
+
+        await waitFor(() => expect(fetchMock).toHaveBeenCalled());
+        await waitFor(() => expect(screen.getByText('* Email already exists').hidden).toBeTruthy());
+
+        await userEvent.type(screen.getByLabelText(/password/i), testUser.password + '{enter}');
+        await userEvent.type(screen.getByLabelText(/repeat/i), testUser.password + '{enter}');
+
+        await waitFor(() => expect(screen.getByText('* Passwords don\'t match').hidden).toBeTruthy());
+
+        fetchMock = global.fetch = jest.fn(async () =>
+            Promise.resolve({ json: async () => Promise.resolve({}) })
+        ) as jest.Mock;
+
+        await userEvent.click(screen.getByRole('button', { name: /sign up/i }));
+
+        await waitFor(() => expect(fetchMock).toHaveBeenCalled());
+        expect(fetchMock.mock.lastCall[0]).toBe('/api/users/');
+        expect(fetchMock.mock.lastCall[1].method).toBe('POST');
+        expect(fetchMock.mock.lastCall[1].body).toContain(JSON.stringify(testUser));
+
+        await waitFor(() => expect(login).toHaveBeenCalled());
+    });
 });
 
-test('invalid email', async () => {
-    render(<SignUp />);
-
-    global.fetch = jest.fn(async () =>
-        Promise.resolve({ json: async () => Promise.resolve({ emailExists: true }) })
-    ) as jest.Mock;
-
-    await userEvent.type(screen.getByRole('textbox', { name: /email/i }), 'email@gmail.com');
-
-    setTimeout(async () => {
-        expect(screen.queryByText('* Email already exists')!.hidden).toBeFalsy();
-    }, 2000);
-});
-
-test('passwords match', async () => {
-    render(<SignUp />);
-
-    await userEvent.type(screen.getByLabelText(/password/i), 'password');
-    await userEvent.type(screen.getByLabelText(/repeat/i), 'password');
-
-    setTimeout(() => {
-        expect(screen.getByText('* Passwords don\'t match').hidden).toBeTruthy();
-    }, 1000);
-});
-
-test('passwords don\'t match', async () => {
-    render(<SignUp />);
-
-    await userEvent.type(screen.getByLabelText(/password/i), 'password');
-    await userEvent.type(screen.getByLabelText(/repeat/i), 'different');
-
-    setTimeout(() => {
-        expect(screen.queryByText('* Passwords don\'t match')!.hidden).toBeFalsy();
-    }, 1000);
-});
-
-test('create user api call', async () => {
-    render(<SignUp />);
-
-    global.fetch = jest.fn(async () =>
-        Promise.resolve({ json: async () => Promise.resolve({ usernameExists: false }) })
-    ) as jest.Mock;
-
-    await userEvent.type(screen.getByRole('textbox', { name: /username/i }), 'username');
-
-    setTimeout(async () => {
-        expect(screen.getByText('* Username already exists').hidden).toBeTruthy();
-    }, 1000);
-
-    global.fetch = jest.fn(async () =>
-        Promise.resolve({ json: async () => Promise.resolve({ emailExists: false }) })
-    ) as jest.Mock;
-
-    await userEvent.type(screen.getByRole('textbox', { name: /email/i }), 'email@gmail.com');
-
-    setTimeout(async () => {
-        expect(screen.getByText('* Email already exists').hidden).toBeTruthy();
-    }, 1000);
-
-    await userEvent.type(screen.getByLabelText(/password/i), 'password');
-    await userEvent.type(screen.getByLabelText(/repeat/i), 'password');
-
-    jest.resetAllMocks();
-
-    const mock = global.fetch = jest.fn(async () =>
-        Promise.resolve({ json: async () => Promise.resolve({}) })
-    ) as jest.Mock;
-
-    setTimeout(async () => {
-        expect(screen.getByText('* Passwords don\'t match').hidden).toBeTruthy();
-    }, 1000);
-
-    await userEvent.click(screen.getByRole('button', { name: /sign up/i }));
-
-    setTimeout(async () => {
-        expect(mock.mock.calls).toHaveLength(1);
-    }, 1000);
-})
