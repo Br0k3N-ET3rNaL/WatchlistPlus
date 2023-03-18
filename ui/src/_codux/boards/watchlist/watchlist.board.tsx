856bbbd6be799fb4a1d0654597e4b184612e46d5
Br0k3N-ET3rNaL <tyliewilie99@gmail.com>
2023-03-18 14:58:16 -0500

    Update watchlist.board.tsx

--- ui/src/_codux/boards/watchlist/watchlist.board.tsx
+++ ui/src/_codux/boards/watchlist/watchlist.board.tsx
@@ -1,7 +1,14 @@
 import { createBoard } from '@wixc3/react-board';
 import Watchlist from '../../../components/watchlist/watchlist';
+import UserContext, { User } from '../../../context';
+
+const user: User = { id: 1, username: 'Username' };
 
 export default createBoard({
     name: 'Watchlist',
-    Board: () => <Watchlist />
+    Board: () => (
+        <UserContext.Provider value={user}>
+            <Watchlist />
+        </UserContext.Provider >
+    )
 });
